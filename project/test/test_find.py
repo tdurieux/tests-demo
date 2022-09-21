f468068025226e9dcb526c77a3277b5982aa5a6b
tdurieux <durieuxthomas@hotmail.com>
2022-09-21 18:17:27 +0200

    add tests

--- project/test/test_find.py
+++ project/test/test_find.py
@@ -12,3 +12,14 @@ def test_any_random_numbers():
   min, max = find([29, 37, 4, 5])
   assert min == 4
   assert max == 37
+
+
+def test_increasing_order():
+  min, max = find([26, 27, 28, 29])
+  assert min == 26
+  assert max == 29
+
+def test_empty_numbers():
+  min, max = find([])
+  assert min is None
+  assert max is None
