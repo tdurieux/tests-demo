36d1ae6102ee90bffacab6a422db11d994537efc
tdurieux <durieuxthomas@hotmail.com>
2022-09-21 18:15:47 +0200

    add tests

--- project/test/test_find.py
+++ project/test/test_find.py
@@ -2,3 +2,13 @@ from ..find import find
 
 def test_find():
   print(find(([29, 28, 27, 26])))
+
+def test_decreasing_order():
+  min, max = find([29, 28, 27, 26])
+  assert min == 26
+  assert max == 29
+  
+def test_any_random_numbers():
+  min, max = find([29, 37, 4, 5])
+  assert min == 4
+  assert max == 37
